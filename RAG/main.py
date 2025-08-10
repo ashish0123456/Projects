6bbd829e29865b349a12f31935a063298ac936cd
ashish.savaliya <ashishsavaliya098@gmail.com>
2025-08-10 11:29:56 +0530

    RAG project using langchain

---
+++ RAG/main.py
@@ -0,0 +1,6 @@
+import langchain
+from langchain.vectorstores import Chroma
+from langchain.embeddings import FastEmbedEmbeddings
+from langchain.text_splitter import characterTextSplitter
+from langchain.document_loaders import TextLoader
+
